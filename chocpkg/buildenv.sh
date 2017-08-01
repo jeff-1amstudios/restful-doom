5e0d0fefd89c119c276abcfe10c38aea34db9855
Jeff Harris <jeff@1amstudios.com>
2017-08-01 14:14:42 -0700

    Removes HACKING, build latest by default

--- chocpkg/buildenv.sh
+++ chocpkg/buildenv.sh
@@ -23,7 +23,7 @@ MAKE_OPTS=
 # By default the stable variant of packages is built unless otherwise
 # requested. If a package name appears inside this array, the "latest"
 # variant of that package (ie. source control HEAD) is built instead.
-LATEST_PACKAGES=(chocolate-doom)
+LATEST_PACKAGES=(restful-doom)
 #LATEST_PACKAGES+=(SDL2 SDL2_image SDL2_mixer SDL2_net)
 
 # On OS X, we must set additional options: build 32-bit binaries, and the
