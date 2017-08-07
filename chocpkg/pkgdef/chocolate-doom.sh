2c490b10c1fbc853205a3acdbf985024e830a7a1
Jeff Harris <jeff@1amstudios.com>
2017-08-07 09:13:19 -0700

    Update chocolate-doom.sh

--- chocpkg/pkgdef/chocolate-doom.sh
+++ chocpkg/pkgdef/chocolate-doom.sh
@@ -3,4 +3,3 @@ dependencies SDL2 SDL2_mixer SDL2_net
 variant latest fetch_git \
     https://github.com/chocolate-doom/chocolate-doom.git sdl2-branch
 build_autotools
-variant rest_api fetch_git https://github.com/jeff-1amstudios/doom-restful-api.git rest_api_sdl2
